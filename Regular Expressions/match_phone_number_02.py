182ed57db6704967c19cccd7113d1bc207f2f8ed
Hristiyann06 <hrisi.haralampiev@gmail.com>
2026-04-01 01:59:03 +0300

    Update match_phone_number_02.py

--- Regular Expressions/match_phone_number_02.py
+++ Regular Expressions/match_phone_number_02.py
@@ -2,6 +2,6 @@ import re
 
 phone_numbers = input()
 
-pattern = re.findall(r"\+359([ -])2\1\d{3}\1\d{4}\b", phone_numbers)
+pattern = re.findall(r"\+359(?:[ -])2(?:[ -])\d{3}(?:[ -])\d{4}\b", phone_numbers)
 
-print(" ".join(pattern))
+print(", ".join(pattern))
